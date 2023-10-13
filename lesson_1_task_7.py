a024f4c3ee440e1c8005ce940ed3f34c3e5d79b1
utanayno <utanayno@gmail.com>
2023-10-13 16:27:06 +0200

    добавление файлов в дз

---
+++ lesson_1_task_7.py
@@ -0,0 +1,90 @@
+def one(x):
+    print("Вывести в консоль", x)
+one(1)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(2)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(3)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(4)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(5)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(6)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(7)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(8)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(9)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(0)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(88005553535)
+
+# Вызовите объявленные функции в таком порядке, чтобы на экране отобразился номер 88005553535.
+
+def one(x):
+    print("Вывести в консоль", x)
+one(8)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(8)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(0)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(0)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(5)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(5)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(5)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(3)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(5)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(3)
+
+def one(x):
+    print("Вывести в консоль", x)
+one(5)
+
---
+++ lesson_1_task_8.py
@@ -0,0 +1,15 @@
+def num(x):
+    print(x)
+    
+num(8)
+num(8)
+num(0)
+num(0)
+num(5)
+num(5)
+num(5)
+num(3)
+num(5)
+num(3)
+num(5)
+
